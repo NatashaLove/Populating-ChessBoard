68652f3a467c91460a54c76d57afb0e707fc0739
Natasha S Love <debora@inbox.ru>
2018-09-18 20:43:26 -0500

    last modifications

--- populateChessBoard.java
+++ populateChessBoard.java
@@ -5,40 +5,48 @@ public class  populateChessBoard {
 	
 	public static void main (String [] args) {
 	
-	// Add pieces to array list.
-	//boolean add(Object o) : This method appends the specified element to the end of the arr list. 
-	//.add - method for arrays - here creates array of objs of class ChessPiece
+		private ArrayList<ChessPiece> pieces = new ArrayList<ChessPiece>(32); //create 32 pieces, 16 for both players
+		
+		// Add pieces to array list.
+		//boolean add(Object o) : This method appends the specified element to the end of the arr list. 
+		//.add - method for arrays - here creates array of objs of class ChessPiece
+	
 	
 		public void createPieces() {
-			for (int i=0; i<8; i++) { //add eight pawns for both colors
-			// constructor: ChessPiece(String color, String name, int row, int column)
+			//add eight pawns for both colors
+			for (int i=0; i<8; i++) { 
+				// constructor: ChessPiece(String color, String name, int row, int column)
 				pieces.add(new ChessPiece("B", "P", 1, i)); //black pawns to 2nd highest row 
 				pieces.add(new ChessPiece("W", "P", 6, i)); //white pawns to 2nd lowest row
 			}
 			// Create new objects (chess pieces) with help of constructor
 			//populate rooks
-			pieces.add(new ChessPiece(1, ChessPiece.ROOK, 0, 0)); //black rook on upper left
-			pieces.add(new ChessPiece(1, ChessPiece.ROOK, 0, 7)); //black rook on upper right
-			pieces.add(new ChessPiece(0, ChessPiece.ROOK, 7, 0)); //white rook on down left
-			pieces.add(new ChessPiece(0, ChessPiece.ROOK, 7, 7)); //white rook on down right
+			pieces.add(new ChessPiece("B", ChessPiece.ROOK, 0, 0)); //black rook on upper left
+			pieces.add(new ChessPiece("B", ChessPiece.ROOK, 0, 7)); //black rook on upper right
+			pieces.add(new ChessPiece("W", ChessPiece.ROOK, 7, 0)); //white rook on down left
+			pieces.add(new ChessPiece("W", ChessPiece.ROOK, 7, 7)); //white rook on down right
 			//populate knights
-			pieces.add(new ChessPiece(1, ChessPiece.KNIGHT, 0, 1)); //black knight on upper 2nd left
-			pieces.add(new ChessPiece(1, ChessPiece.KNIGHT, 0, 6)); //black knight on upper 2nd right
-			pieces.add(new ChessPiece(0, ChessPiece.KNIGHT, 7, 1)); //white knight on down 2nd left
-			pieces.add(new ChessPiece(0, ChessPiece.KNIGHT, 7, 6)); //white knight on down 2nd right
+			pieces.add(new ChessPiece("B", ChessPiece.KNIGHT, 0, 1)); //black knight on upper 2nd left
+			pieces.add(new ChessPiece("B", ChessPiece.KNIGHT, 0, 6)); //black knight on upper 2nd right
+			pieces.add(new ChessPiece("W", ChessPiece.KNIGHT, 7, 1)); //white knight on down 2nd left
+			pieces.add(new ChessPiece("W", ChessPiece.KNIGHT, 7, 6)); //white knight on down 2nd right
 			//populate bishops
-			pieces.add(new ChessPiece(1, ChessPiece.BISHOP, 0, 2)); //black bishop on upper 3rd left
-			pieces.add(new ChessPiece(1, ChessPiece.BISHOP, 0, 5)); //black bishop on upper 3rd right
-			pieces.add(new ChessPiece(0, ChessPiece.BISHOP, 7, 2)); //white bishop on down 3rd left
-			pieces.add(new ChessPiece(0, ChessPiece.BISHOP, 7, 5)); //white bishop on down 3rd right
+			pieces.add(new ChessPiece("B", ChessPiece.BISHOP, 0, 2)); //black bishop on upper 3rd left
+			pieces.add(new ChessPiece("B", ChessPiece.BISHOP, 0, 5)); //black bishop on upper 3rd right
+			pieces.add(new ChessPiece("W", ChessPiece.BISHOP, 7, 2)); //white bishop on down 3rd left
+			pieces.add(new ChessPiece("W", ChessPiece.BISHOP, 7, 5)); //white bishop on down 3rd right
 			//populate queens
-			pieces.add(new ChessPiece(1, ChessPiece.QUEEN, 0, 3)); //black queen on upper 4rd left
-			pieces.add(new ChessPiece(0, ChessPiece.QUEEN, 7, 3)); //white queen on down 4rd left
+			pieces.add(new ChessPiece("B", ChessPiece.QUEEN, 0, 3)); //black queen on upper 4rd left
+			pieces.add(new ChessPiece("W", ChessPiece.QUEEN, 7, 3)); //white queen on down 4rd left
 			//populate kings
-			pieces.add(new ChessPiece(1, ChessPiece.KING, 0, 4)); //black king on upper 5th left
-			pieces.add(new ChessPiece(0, ChessPiece.KING, 7, 4)); //white king on down 5th left
+			pieces.add(new ChessPiece("B", ChessPiece.KING, 0, 4)); //black king on upper 5th left
+			pieces.add(new ChessPiece("W", ChessPiece.KING, 7, 4)); //white king on down 5th left
 		}
 
+		// prints all the elements available in list 
+        for (ChessPiece piece : pieces) { 
+            System.out.println(" " + piece + " "); 
+        } 
 	
 	}
  }
